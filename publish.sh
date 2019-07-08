51b32f6d680258c638dd796e608c9a28a4776b49
Iggy Lake <iglake@yandex.com>
2019-07-08 11:03:09 +0200

    tuned the publishing script and added the js folder

--- publish.sh
+++ publish.sh
@@ -2,12 +2,15 @@
 
 tic=$(date +%s)
 spot=$(perl -S spot.pl | ipfs add -Q --hash sha1)
-qm=$(ipfs add -Q --hash sha3-224 -r --cid-base=base58btc .)
 echo spot: $spot
+git commit -a
+rm -f *.org {css,js}/*.org
+qm=$(ipfs add -Q --hash sha3-224 -r --cid-base=base58btc .)
 echo qm: $qm
 echo $tic: $qm >> qm.log
+echo " - $qm" >> mutable.yml
 mv index.htm index.htm~
-perl -pe "s,/ipfs/(:?z6cYN|Qm)[^\"]+,/ipfs/$qm," index.htm~ > index.htm
+perl -pe "s,/ipfs/(:?z6cYN|Qm)[^/\" ]+,/ipfs/$qm," index.htm~ > index.htm
 rm -f index.htm~
 
 
