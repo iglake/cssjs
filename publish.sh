fc4ed29695f645ae13fc90f57da8af00c6dbd64f
Iggy Lake <iglake@yandex.com>
2019-08-09 18:32:04 +0200

    publishing on 1565368193 for v1.2.7

--- publish.sh
+++ publish.sh
@@ -1,10 +1,11 @@
 #
 
+git pull
+find . -name \*.org -delete
 tic=$(date +%s)
 spot=$(perl -S spot.pl | ipfs add -Q --hash sha1)
 echo spot: $spot
-git commit -a
-rm -f *.org {css,js}/*.org
+ver=$($HOME/bin/version index.htm | xyml scheduled)
 qm=$(ipfs add -Q --hash sha3-224 -r --cid-base=base58btc .)
 echo qm: $qm
 echo $tic: $qm >> qm.log
@@ -13,4 +14,11 @@ mv index.htm index.htm~
 perl -pe "s,/ipfs/(:?z6cYN|Qm)[^/\" ]+,/ipfs/$qm," index.htm~ > index.htm
 rm -f index.htm~
 
-
+if git commit -a -m "publishing on $tic for $ver"; then
+date=$(date +%D)
+rev=$(git rev-parse --short HEAD)
+git tag -f -a $ver -m "tagging $rev on $date"
+echo "$ver: $date ($rev)" > VERSION
+find .git/objects -type f -exec ipfs dag put --format=git --input-enc=zlib {} \; > hashes
+git push --tags
+fi
